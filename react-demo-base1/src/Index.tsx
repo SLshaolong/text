3bc19b77468c55e3b463a032f1eded9e9e0d63d7
shaolong <3037302304@qq.com>
2023-01-08 16:20:11 +0800

    a

--- react-demo-base1/src/Index.tsx
+++ react-demo-base1/src/Index.tsx
@@ -1,8 +1,10 @@
+import Fragments from "./component2/Fragments";
+
 export default function Index ()
 {
 
     return <div>
         <h3>另一个组件</h3>
-
+        <Fragments/>
     </div>
 }
---
+++ react-demo-base1/src/component2/Fragments.tsx
@@ -0,0 +1,12 @@
+import React ,{Fragment} from "react";
+export default class Fragments extends React.Component{
+    render(): React.ReactNode {
+        return (
+            <Fragment>
+                <p>你好</p>
+                <p>你好</p>
+
+            </Fragment>
+        )
+    }
+}
